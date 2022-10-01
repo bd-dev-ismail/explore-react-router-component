839781ab928a2f23710b32fc3d91963274799366
bd-dev-ismail <ismailratul7@gmail.com>
2022-10-02 04:04:03 +0600

    added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './components/About';
+import Header from './components/Header';
+import Home from './components/Home';
+import Products from './components/Products';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/products' element={<Products></Products>}></Route>
+      </Routes>
     </div>
   );
 }
